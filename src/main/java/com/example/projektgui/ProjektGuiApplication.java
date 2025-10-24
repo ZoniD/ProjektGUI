5cc71d51b8138783e19c5617df08d6e553f6f4f2
ZoniD <deba0001@stud.kea.dk>
2025-10-24 14:25:47 +0200

    Madspild-GUI-Projekt

---
+++ src/main/java/com/example/projektgui/ProjektGuiApplication.java
@@ -0,0 +1,13 @@
+package com.example.projektgui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektGuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektGuiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projektgui/ProjektGuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projektgui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektGuiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
